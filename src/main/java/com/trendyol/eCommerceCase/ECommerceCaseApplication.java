62b389626f64baa6cc03b716984712ccea8c85d3
almtl55 <alimutlu_5529@icloud.com>
2021-03-03 19:52:01 +0300

    initial commit

---
+++ src/main/java/com/trendyol/eCommerceCase/ECommerceCaseApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.eCommerceCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceCaseApplication.class, args);
+	}
+
+}
